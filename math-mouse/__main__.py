4622eb36b07a813b1a9810afba625cb85f8c7e1e
Chih-Kai Ting <tingken0214@gmail.com>
2020-12-31 01:05:36 +0800

    Feat(Linear Least Square): Add weight calculattion and rss function.

--- math-mouse/__main__.py
+++ math-mouse/__main__.py
@@ -2,7 +2,7 @@ import numpy as np
 from logger import error, Logger
 
 
-def basic_linear(_x, _b):
+def basic_linear(x, b):
     """
     Formula:
     Y_hat = X^T * B_hat
@@ -10,8 +10,31 @@ def basic_linear(_x, _b):
     X: Input Data/Training Data
     B_hat: Coefficients/Weights
     """
-    _y = _b@_x.T
-    return _y
+    y = x@b
+    return y
+
+
+def rss(x, y, b):
+    """
+    Residual Sum of Squares:
+    RSS(B) = sum(Y - X^T*B)^2
+           = (y - XB)^T (y - XB)
+    """
+    y_hat = basic_linear(x, b)
+    diff = y - y_hat
+    diff_square = np.square(diff)
+    rss = np.sum(diff_square)
+    return rss
+
+
+def get_weight_with_ls(x, y):
+    """
+    Get Weight with Least Square:
+    We can get weights with the method of least square given X: input data and Y: true value.
+    B^hat = (X^T X)^(-1) X^T y
+    """
+    b = np.linalg.inv(x.T@x)@x.T@y
+    return b
 
 
 if __name__ == "__main__":
@@ -19,9 +42,15 @@ if __name__ == "__main__":
     main_logger.info("Math Mouse started the calculation!")
     try:
         x = np.arange(15).reshape(3, 5)
-        b = np.arange(5).reshape(1, 5)
-        y = basic_linear(x, b)
-        main_logger.info("The Prediction is : {}".format(y))
+        y = np.arange(3).reshape(3, 1)
+        b_hat = get_weight_with_ls(x, y)
+        y_hat = basic_linear(x, b_hat)
+        current_rss = rss(x, y, b_hat)
+        main_logger.info("The X is :\n{}".format(x))
+        main_logger.info("The Y is :\n{}".format(y))
+        main_logger.info("The Weight with Least Square is :\n{}".format(b_hat))
+        main_logger.info("The RSS is :\n{}".format(current_rss))
+        main_logger.info("The Prediction is :\n{}".format(y_hat))
     except Exception as exc_info:
         messages = error.error_logging(exc_info)
         main_logger.error(messages)
