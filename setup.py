8b6270b87ccc3b6a4db0e121d870497641f530d1
Chih-Kai Ting <tingken0214@gmail.com>
2021-03-06 18:57:28 +0800

    chore(setup): add environment setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def setup_package():
+    setup(
+        name="math-mouse",
+        description="A mouse can do math.",
+        url="https://github.com/daohuei/math-mouse",
+        author="daohuei",
+        author_email="tingken0214@gmail.com",
+        maintainer="daohuei",
+        maintainer_email="tingken0214@gmail.com",
+        packages=find_packages(exclude=["data", "model"]),
+        license="MIT",
+        # Note: many of these required packages are included in base python
+        # but are listed here because different linux distros use custom
+        # python installations.  And users can remove packages at any point
+        install_requires=[
+            "typer",
+            "numpy",
+            "pandas",
+            "sklearn",
+            "plotly"
+        ],
+    )
+
+
+if __name__ == "__main__":
+    setup_package()
